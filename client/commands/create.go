3e2334e2dbda3c7f695e003a98f3bc2f5fa4f6a8
adk-saugat <adhikarisaugat34@gmail.com>
2025-12-10 18:01:49 -0600

    created .stash file to store changes

--- client/commands/create.go
+++ client/commands/create.go
@@ -1,5 +1,45 @@
 package commands
 
+import (
+	"log"
+	"os"
+)
+
 func Create(){
-	
+	// creating the .stash folder
+	exists, err := folderExists(".stash")
+	if err != nil {
+		log.Fatal("Error: Couldnot check folders existence.")
+	}
+
+	if !exists {
+		err = os.Mkdir(".stash", 0755)
+		if err != nil {
+			log.Fatal("Error: Couldnot create folder.")
+		}
+	}
+
+	// creating the store folder
+	exists, err = folderExists(".stash/store")
+	if err != nil {
+		log.Fatal("Error: Couldnot check folders existence.")
+	}
+
+	if !exists {
+		err = os.Mkdir("./.stash/store", 0755)
+		if err != nil {
+			log.Fatal("Error: Couldnot create.")
+		}
+	}
+}
+
+func folderExists(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err == nil {
+		return info.IsDir(), nil 
+	}
+	if os.IsNotExist(err) {
+		return false, nil 
+	}
+	return false, err 
 }
--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,5 @@
 module github.com/adk-saugat/stash
 
 go 1.24.5
+
+require github.com/sergi/go-diff v1.4.0 // indirect
